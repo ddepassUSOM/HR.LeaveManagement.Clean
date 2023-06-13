98e55c499c85d7969b181c18920898297901ca83
Dwight Depass <Dwight.DePass@cunamutual.com>
2023-06-13 07:29:29 -0500

    Updated

---
+++ HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -0,0 +1,14 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+	public class LeaveRequestRepository : GenericRespository<LeaveRequest>, ILeaveRequestRepository
+	{
+		public LeaveRequestRepository(HrDatabaseContext context) : base(context)
+		{
+		}
+				
+	}
+}
