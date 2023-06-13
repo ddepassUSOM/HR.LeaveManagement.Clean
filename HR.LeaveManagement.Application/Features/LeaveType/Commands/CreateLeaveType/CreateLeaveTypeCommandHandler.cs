d0fd26ff13c8fba8ef979c535662083d0d37f7e2
Dwight Depass <Dwight.DePass@cunamutual.com>
2023-06-13 06:50:38 -0500

    update

--- HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -22,6 +23,13 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeave
         public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
 		{
 			//validate incoming data
+			var validator = new CreateLeaveTypeCommandValidator(_leaveTypeRepository);	
+			var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+			if (validationResult.Errors.Any())
+			{
+				throw new BadRequestException("Invalid Leavetype", validationResult);
+			}
 
 			//convert to domain entity object
 			var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
