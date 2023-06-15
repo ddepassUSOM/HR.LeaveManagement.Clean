61ce46e40afb3216b79140fe21e7bc36a6400648
Dwight Depass <Dwight.DePass@cunamutual.com>
2023-06-15 14:41:02 -0500

    updated

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocations/GetLeaveAllocationListHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocations
+{
+	public class GetLeaveAllocationListHandler : IRequestHandler<GetLeaveAllocationListQuery,
+		List<LeaveAllocationDto>>
+	{
+		private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+		private readonly IMapper _mapper;
+
+		public GetLeaveAllocationListHandler(ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper)
+        {
+			this._leaveAllocationRepository = leaveAllocationRepository;
+			this._mapper = mapper;
+		}
+        public Task<List<LeaveAllocationDto>> Handle(GetLeaveAllocationListQuery request, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
