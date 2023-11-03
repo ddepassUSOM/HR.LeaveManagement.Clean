3b5b9e0b84358cffca79b736d65ad0475403a37a
Dwight Depass <Dwight.DePass@cunamutual.com>
2023-11-03 10:10:57 -0500

    Setup Leave Type Management Service

--- HR.LeaveManagement.BlazorUI/Program.cs
+++ HR.LeaveManagement.BlazorUI/Program.cs
@@ -1,9 +1,11 @@
+
 using HR.LeaveManagement.BlazorUI;
 using HR.LeaveManagement.BlazorUI.Contracts;
 using HR.LeaveManagement.BlazorUI.Services;
 using HR.LeaveManagement.BlazorUI.Services.Base;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Reflection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,5 +18,7 @@ builder.Services.AddHttpClient<IClient, Client>(Client => Client.BaseAddress = n
 builder.Services.AddScoped<ILeaveTypeService, LeaveTypeService>();
 builder.Services.AddScoped<ILeaveRequestService, LeaveRequestService>();
 builder.Services.AddScoped<ILeaveAllocationService, LeaveAllocationService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 await builder.Build().RunAsync();
